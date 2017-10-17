c3448f0b87599da8cf62f4fd8c9664adae336f82
d.korolenok <d.korolenok@discretemind.com>
2017-10-17 11:45:42 +0300

    refactor address component

--- server/static/src/app/components/address/address.component.ts
+++ server/static/src/app/components/address/address.component.ts
@@ -8,38 +8,14 @@ import { ApiService } from '../../services/api.service';
   templateUrl: './address.component.html',
   styleUrls: ['./address.component.css']
 })
-export class AddressComponent implements OnDestroy {
+export class AddressComponent {
   private address: string;
   private routeSubscription: Subscription;
-  private info: any;
-  rows: Array< { index: string, block: number, hash: string}> = [];
-  columns = [
-    { prop: 'index' },
-    { prop: 'block' },
-    { prop: 'hash' },
-  ];
-
 
   constructor(private api: ApiService, private route: ActivatedRoute ) {
     this.routeSubscription = route.params.subscribe(params=> {
-      this.address=params['id'];
-      this.api.getAddressInfoBTC(this.address).subscribe((data: any) => {
-        let info = JSON.parse(data._body);
-        this.info = info;
-        this.changeRows(this.info.txs)
-      })
+      this.address = params['id'];
     })
-  }
-
-  ngOnDestroy() {
-    this.routeSubscription.unsubscribe();
-  }
-
-  public changeRows(transactions: any) {
-    this.rows = [];
-    for (let i=0; i<=transactions.length-1; i++) {
-      this.rows.push({'index': transactions[i].tx_index, 'block': transactions[i].block_height, 'hash': transactions[i].hash})
-    }
   }
 
 }
